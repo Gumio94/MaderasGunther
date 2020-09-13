ccb602cbb7df19987cd6201af5eaf45d9a4f7248
NicolasAndrada <nicolas.andrada2@gmail.com>
2020-09-12 21:14:59 -0300

    Update carga_formulario.php

--- carga_formulario.php
+++ carga_formulario.php
@@ -12,20 +12,18 @@ $consulta = "INSERT INTO consulta_de_cliente (nombre, email, telefono, mensaje)
 
 $comprobar_consulta = $conectar -> query($consulta);
 
-/*if ($comprobar_consulta){
+if ($comprobar_consulta){
     echo " LOS DATOS SE GRABARON EN LA DB";
     echo "<a href= contacto.php></br>volver a Página de contactos</a>";
 }else{
     echo " ERROR EN LA CONSULTA, NO SE PUDO REALIZAR";
 }
-$enviaremail= mail("franciscosrc03@gmail.com;nicolas.andrada2@gmail.com ",$nombre, $emal, $telefono, utf8_decode($mensaje));
+$enviaremail= mail("contacto@maderasgunther.com.ar",$nombre, $emal, $telefono, utf8_decode($mensaje));
 if ($enviaremail){
     echo " el email fue enviado";
    
 }else{
     echo " ERROR el email NO SE PUDO ENVIAR";
-}*/
-
-
+}
 
 ?>
