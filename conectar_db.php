c1517de39c44eae5997af6a14994e6b36c829fc6
NicolasAndrada <nicolas.andrada2@gmail.com>
2020-09-12 21:49:50 -0300

    Update conectar_db.php

--- conectar_db.php
+++ conectar_db.php
@@ -1,7 +1,7 @@
 <?php 
 
 //conectar con el servidor
-$conectar = new mysqli ("localhost", "root", "","cliente");
+$conectar = new mysqli ("92.249.44.52", "u761283263_gunther", "Maderasgunther20","u761283263_maderas");
 
 
 
