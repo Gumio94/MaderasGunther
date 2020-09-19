be8993393b62ec32892b09e5c1c419c7ac3e5dcd
NicolasAndrada <nicolas.andrada2@gmail.com>
2020-09-19 18:40:42 -0300

    d

---
+++ tipos_de_maderas.php
@@ -0,0 +1,19 @@
+<?php include "inc/header.php"?>
+
+<div class="container-fluid cuerpo">
+    <div class="row justify-content-md-center">
+        <div class="col-12 py-2 barra">
+            <?php include "inc/navbar.php"?>
+        </div>
+        <ul class="list-group">
+          <li class="list-group-item">Varillas de alambrado</li>
+          <li class="list-group-item">Anchico</li>
+          <li class="list-group-item">Guayaubira</li>
+          <li class="list-group-item">Eucaliptus rostrata</li>
+          <li class="list-group-item">Curupay</li>
+          <li class="list-group-item">Lapacho</li>
+          <li class="list-group-item">Rabo</li>
+        </ul>
+    </div>
+</div>   
+<?php include "inc/footer.php"?>
