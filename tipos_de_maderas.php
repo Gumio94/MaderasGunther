b1c97476dc30117a88024ed2692562a73042f57b
FranciscoRodriguezC <franciscosrc03@gmail.com>
2020-09-23 01:39:10 +0200

    armado de carrusel
    se cargaron las fotos para el carrusel de arte rústico

--- tipos_de_maderas.php
+++ tipos_de_maderas.php
@@ -85,10 +85,43 @@
 
             <div id = "arte_rustico" class ="p-4">
             <h2 class="blanco" osw > ARTE RÚSTICO</h2>
-                <ul class="list-group">
-                    <li class="list-group-item">imágenes de arte rústico</li>
-                   
-                </ul>
+
+            <div class="col-md-12 mb-3 px-7 py-4 bg-negro">
+            
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="img/arteRustico/mesadi1.jpg" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img class="d-block w-100" src="img/arteRustico/mesadi2.jpg" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img class="d-block w-100" src="img/arteRustico/mesadi3.jpg" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img class="d-block w-100" src="img/arteRustico/mesadi4.jpg" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img class="d-block w-100" src="img/arteRustico/puertadi.jpg" alt="...">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                    </div>
+
+            </div>
+
+
+
+
+                
             </div>
     </div>
 </div>   
