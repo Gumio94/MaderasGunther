e0db356e97fff1130e30415fe2074eb301542d15
FranciscoRodriguezC <franciscosrc03@gmail.com>
2020-09-23 02:24:22 +0200

    error mio
    corregí un error, acualice varias veses y el index seguia el primero que teníamos, cuando actalice se rompió todo , es por eso que no veia las 16 fotos.

--- index.php
+++ index.php
@@ -1,36 +1,112 @@
+
 <?php include "inc/header.php"?>
+
+<div class="degrade">
 <body>
 <div class="container-fluid cuerpo">
     <div class="row justify-content-md-center">
-        <div class="col-sm-7 py-3 barra">
+        <div class="col-12 py-2 barra">
+        
             <?php include "inc/navbar.php"?>
         </div>
-        <div class="col-md-7 py-3 my-4">
+        <div class="col-md-7 pb-2 mt-2 mb-4">
+        
             <h1 class="text-center amarillo">Bienvenidos a</h1>
             <h1 class="text-center">Maderas Günther</h1>
         </div>
+        
         <div class="col-md-7 pb-3 ">
             <div class="d-flex justify-content-center">
                 <h2 class="negro bg-amarillo px-3 py-1 d-inline">¿Quienes somos?</h2>
             </div>
-            <p class="text-center my-4">Lorem ipgsum dolor sit amet, consectetur adipisicing elit. Minus quas eveniet ad quod sunt necessitatibus repellat laudantium amet tempora tenetur possimus consequatur nesciunt iure, expedita, ipsum molestiae, harum error repellendus?</p>
-        </div>
-        <div class="col-md-6 mb-3 px-5 py-2 bg-negro">
-            <div id="carouselExampleSlidesOnly" class="carousel slide mx-auto" data-ride="carousel">
+                <p class="text-center my-4">Provenimos de una familia que se radicó en el Alto Paraná de Misiones en 1920.
+Poseemos una larga experiencia familiar en la actividad maderil.
+Trabajamos con todo tipo de maderas: Duras, semiduras y las implantadas.
+Todas las maderas cumplen con el protocolo normativo de los planes de manejo forestal.</p>
+            
+            </div>
+            <div class="col-md-8 col-lg-6 mb-3 px-1 py-1 bg-negro">
+            <div id="fotos" class="carousel slide mx-auto" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#fotos" data-slide-to="0" class="active"></li>
+                    <li data-target="#fotos" data-slide-to="1"></li>
+                    <li data-target="#fotos" data-slide-to="2"></li>
+                    <li data-target="#fotos" data-slide-to="3"></li>
+                    <li data-target="#fotos" data-slide-to="4"></li>
+                    <li data-target="#fotos" data-slide-to="5"></li>
+                    <li data-target="#fotos" data-slide-to="6"></li>
+                    <li data-target="#fotos" data-slide-to="7"></li>
+                    <li data-target="#fotos" data-slide-to="8"></li>
+                    <li data-target="#fotos" data-slide-to="9"></li>
+                    <li data-target="#fotos" data-slide-to="10"></li>
+                    <li data-target="#fotos" data-slide-to="11"></li>
+                    <li data-target="#fotos" data-slide-to="12"></li>
+                    <li data-target="#fotos" data-slide-to="13"></li>
+                    <li data-target="#fotos" data-slide-to="14"></li>
+                </ol>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="img/gallery/camiondi1.jpg" class="d-block w-100" alt="...">
+                        <img src="img/gallery/1.jpeg" srcset="img/gallery/1min.jpeg 300w" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="img/2.jpg" class="d-block w-100" alt="...">
+                        <img src="img/gallery/2.jpeg" srcset="img/gallery/2min.jpeg 300w" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="img/3.jpg" class="d-block w-100" alt="...">
+                        <img src="img/gallery/3.jpeg" srcset="img/gallery/3min.jpeg 300w" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/gallery/5.jpeg" srcset="img/gallery/5min.jpeg 300w" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/gallery/6.jpeg" srcset="img/gallery/6min.jpeg 300w" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/gallery/7.jpeg" srcset="img/gallery/7min.jpeg 300w" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/gallery/8.jpeg" srcset="img/gallery/8min.jpeg 300w" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/gallery/9.jpeg" srcset="img/gallery/9min.jpeg 300w" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/gallery/10.jpeg" srcset="img/gallery/10min.jpeg 300w" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/gallery/11.jpeg" srcset="img/gallery/11min.jpeg 300w" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/gallery/12.jpeg" srcset="img/gallery/12min.jpeg 300w" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/gallery/13.jpeg" srcset="img/gallery/13min.jpeg 300w" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/gallery/14.jpeg" srcset="img/gallery/14min.jpeg 300w" class="d-block w-100" alt="...">
+                       
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/gallery/15.jpeg" srcset="img/gallery/15min.jpeg 300w" class="d-block w-100" alt="...">
+                        
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/gallery/16.jpeg" srcset="img/gallery/16min.jpeg 300w" class="d-block w-100" alt="...">
+                      
                     </div>
                 </div>
+                <a class="carousel-control-prev" href="#fotos" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Anterior</span>
+                </a>
+                <a class="carousel-control-next" href="#fotos" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Siguiente</span>
+                </a>
             </div>
         </div>
     </div>
+</div>
+</div>
 </div>  
 
 </body>
